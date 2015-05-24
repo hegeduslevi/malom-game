49aebbeef7423b5062dddd691c4d9ed4be02d448
Levente Hegedűs <berkogep@gmail.com>
2015-05-24 19:47:27 +0200

    Licence updated

--- src/main/java/pkg/TableRowType.java
+++ src/main/java/pkg/TableRowType.java
@@ -1,5 +1,28 @@
 package pkg;
 
+/*
+ * #%L
+ * Malom-Game
+ * %%
+ * Copyright (C) 2015 Berkó-gép
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
 /***
  * JTable sorainak elkészítéséhez szükséges osztály. Egy játékos adatait tartalmaza.
  */
